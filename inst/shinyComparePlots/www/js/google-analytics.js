ba4a1aa5b2d6652fe551486134791af8c268b08b
Vinodh N. Rajapakse <vinodh.rajapakse@gmail.com>
2016-09-06 14:33:53 -0400

    Added code to support GoogleAnalytics tracking of internal group application.

--- inst/shinyComparePlots/www/js/google-analytics.js
+++ inst/shinyComparePlots/www/js/google-analytics.js
@@ -5,3 +5,21 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-57486113-2', 'auto');
 ga('send', 'pageview');
+
+$(document).ready(function() {
+  //$('input, textarea, select').change(function() {
+  //  var tmp = {"db": "a", "p": "e", "items": ["b", "c", "d"]};
+  //  console.log("ignore : " + JSON.stringify(tmp));
+  //  ga('send', 'event', 'ignore', JSON.stringify(tmp));
+  //});
+  
+  // For tracking on internal (NCI/DTB) group application only.
+  $("#rm-predIds").change(function() { 
+  	var dataset = $("#rm-dataset").val();
+  	var responseId = $("#rm-responseId").val();
+  	var predIds = $("#rm-predIds").val();
+  	var tmp = {"d": dataset, "r": responseId, "p": predIds};
+  	//console.log("rmModels : " + JSON.stringify(tmp));
+    ga('send', 'event', 'rmModels', JSON.stringify(tmp));
+  });
+});
