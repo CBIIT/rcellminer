c1020172e866954a142f50319b8b6416c81605cf
Augustin Luna <cannin@gmail.com>
2017-11-09 11:34:07 -0500

    Update rcellminer.R

--- R/rcellminer.R
+++ R/rcellminer.R
@@ -6,3 +6,6 @@
 .onLoad <- function(lib, pkg){
 }
 
+.onAttach <- function(libname, pkgname){
+    packageStartupMessage('Consider citing this package: Luna A, et al. rcellminer: exploring molecular profiles and drug response of the NCI-60 cell lines in R. PMID: 26635141; citation("rcellminer")')
+}
