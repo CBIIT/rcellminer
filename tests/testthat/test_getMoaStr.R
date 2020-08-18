ce9092179de0e29fbdbffc5601861f8c26084059
Elloumi <elloumif@nci-02048674-ml.nih.gov>
2020-08-18 17:18:27 -0400

    Edits related for the new NCI60 data updates

--- tests/testthat/test_getMoaStr.R
+++ tests/testthat/test_getMoaStr.R
@@ -1,6 +1,6 @@
 test_that("getMoaStr() returns all applicable MOA strings", {
-	expect_identical(getMoaStr("609699"), c("609699"="T1"))
-	expect_identical(getMoaStr("123127"), c("123127"="T2"))
+	expect_identical(getMoaStr("609699"), c("609699"="TOP1"))
+	expect_identical(getMoaStr("123127"), c("123127"="TOP2"))
 	expect_identical(getMoaStr("762"), c("762"="A7,AlkAg"))
 	
 	expect_true(is.na(getMoaStr("0")))
